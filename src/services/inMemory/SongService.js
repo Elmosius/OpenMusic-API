cc7278ed828f58a9532cf5758af4b8e3af2b3271
Elmosius <2272008@maranatha.ac.id>
2025-05-11 14:44:56 +0700

    Create SongService.js

---
+++ src/services/inMemory/SongService.js
@@ -0,0 +1,57 @@
+class SongService {
+  constructor() {
+    this._songs = [];
+  }
+
+  addSong({ title, year, genre, performer, duration, albumId }) {
+    const id = `song-${nanoid(16)}`;
+    const newSong = { id, title, year, genre, performer, duration, albumId };
+    this._songs.push(newSong);
+
+    const isSuccess = this._songs.filter((song) => song.id === id).length > 0;
+
+    if (!isSuccess) {
+      throw new Error("Song gagal ditambahkan");
+    }
+
+    return id;
+  }
+
+  getSongs() {
+    return this._songs.map((song) => ({
+      id: song.id,
+      title: song.title,
+      performer: song.performer,
+    }));
+  }
+
+  getSongById(id) {
+    const song = this._songs.filter((s) => s.id === id)[0];
+
+    if (!song) {
+      throw new Error("Lagu tidak ditemukan");
+    }
+
+    return song;
+  }
+
+  editSongById(id, { title, year, genre, performer, duration, albumId }) {
+    const index = this._songs.findIndex((song) => song.id === id);
+
+    if (index === -1) {
+      throw new Error("Gagal memperbarui lagu. Id tidak ditemukan");
+    }
+
+    this._songs[index] = { ...this._songs[index], title, year, genre, performer, duration, albumId };
+  }
+
+  deleteSongById(id) {
+    const index = this._songs.findIndex((song) => song.id === id);
+
+    if (index === -1) {
+      throw new Error("Lagu gagal dihapus. Id tidak ditemukan");
+    }
+
+    this._songs.splice(index, 1);
+  }
+}
