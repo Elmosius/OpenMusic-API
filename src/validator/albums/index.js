aef512d99ec563797612acb8c0dd93c03749d241
Elmosius <2272008@maranatha.ac.id>
2025-05-11 15:19:02 +0700

    create validator albums

---
+++ src/validator/albums/index.js
@@ -0,0 +1,12 @@
+const { AlbumPayloadSchema } = require("./schema");
+
+const AlbumsValidator = {
+  validateAlbumPayload: (payload) => {
+    const validatonResult = AlbumPayloadSchema.validate(payload);
+    if (validatonResult.error) {
+      throw new InvariantError(validatonResult.error.message);
+    }
+  },
+};
+
+module.exports = AlbumsValidator;
