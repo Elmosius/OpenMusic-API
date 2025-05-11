ff19d8c6224ccad2caf412e292df83861a42c6fc
Elmosius <2272008@maranatha.ac.id>
2025-05-11 15:08:37 +0700

    Update handler.js

--- src/api/album/handler.js
+++ src/api/album/handler.js
@@ -9,13 +9,53 @@ class AlbumsHandler {
     this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler.bind(this);
   }
 
-  async postAlbumHandler(request, h) {}
+  async postAlbumHandler(request, h) {
+    const { name, year } = request.payload;
 
-  async getAlbumHandler() {}
+    const albumId = this._service.addAlbum({ name, year });
+    const response = h.response({
+      status: "success",
+      data: {
+        albumId,
+      },
+    });
 
-  async putAlbumByIdHandler(request, h) {}
+    response.code(201);
+    return response;
+  }
 
-  async deleteAlbumByIdHandler(request, h) {}
+  async getAlbumHandler() {
+    const albums = this._service.getAlbums();
+    return {
+      status: "success",
+      data: {
+        albums,
+      },
+    };
+  }
+
+  async putAlbumByIdHandler(request, h) {
+    const { id } = request.params;
+    const { name, year } = request.payload;
+
+    this._service.editAlbumById(id, { name, year });
+
+    return {
+      status: "success",
+      message: "Album berhasil diperbarui",
+    };
+  }
+
+  async deleteAlbumByIdHandler(request, h) {
+    const { id } = request.params;
+
+    this._service.deleteAlbumById(id);
+
+    return {
+      status: "success",
+      message: "Album berhasil dihapus",
+    };
+  }
 }
 
 module.exports = AlbumsHandler;
