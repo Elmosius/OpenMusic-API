6373b59d0fd52f0192a9fb6e846eedb9a0bfd7cf
Elmosius <2272008@maranatha.ac.id>
2025-05-11 17:47:30 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+const albums = require("./api/album");
+const songs = require("./api/song");
+const AlbumsValidator = require("./validator/albums");
+const SongsValidator = require("./validator/songs");
+const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
+  const albumService = require("./services/postgres/AlbumsService");
+  const songService = require("./services/postgres/SongsService");
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -11,6 +19,38 @@ const init = async () => {
         origin: ["*"],
       },
     },
+  });
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumService,
+        validator: AlbumsValidator,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songService,
+        validator: SongsValidator,
+      },
+    },
+  ]);
+
+  server.ext("onPreResponse", (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: "fail",
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
