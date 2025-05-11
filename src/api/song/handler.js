be5d8c93ed6e7d4d60306eefe28bfe967ad4f45b
Elmosius <2272008@maranatha.ac.id>
2025-05-11 15:08:34 +0700

    Update handler.js

--- src/api/song/handler.js
+++ src/api/song/handler.js
@@ -9,15 +9,65 @@ class SongsHandler {
     this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
   }
 
-  async postSongHandler(request, h) {}
+  async postSongHandler(request, h) {
+    const { title, year, genre, performer, duration, albumId } = request.payload;
+    const songId = this._service.addSong({ title, year, genre, performer, duration, albumId });
 
-  async getSongHandler() {}
+    const response = h.response({
+      status: "success",
+      data: {
+        songId,
+      },
+    });
 
-  async getSongByIdHandler(request, h) {}
+    response.code(201);
+    return response;
+  }
 
-  async putSongByIdHandler(request, h) {}
+  async getSongHandler() {
+    const songs = this._service.getSongs();
+    return {
+      status: "success",
+      data: {
+        songs,
+      },
+    };
+  }
 
-  async deleteSongByIdHandler(request, h) {}
+  async getSongByIdHandler(request, h) {
+    const { id } = request.params;
+    const song = this._service.getSongById(id);
+
+    return {
+      status: "success",
+      data: {
+        song,
+      },
+    };
+  }
+
+  async putSongByIdHandler(request, h) {
+    const { id } = request.params;
+    const { title, year, genre, performer, duration, albumId } = request.payload;
+
+    this._service.editSongById(id, { title, year, genre, performer, duration, albumId });
+
+    return {
+      status: "success",
+      message: "Song berhasil diperbarui",
+    };
+  }
+
+  async deleteSongByIdHandler(request, h) {
+    const { id } = request.params;
+
+    this._service.deleteSongById(id);
+
+    return {
+      status: "success",
+      message: "Song berhasil dihapus",
+    };
+  }
 }
 
 module.exports = SongsHandler;
