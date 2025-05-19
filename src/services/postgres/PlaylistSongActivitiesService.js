e6f91b65a85e607fe10a8acd53a886f992e3187b
Elmosius <2272008@maranatha.ac.id>
2025-05-19 12:08:32 +0700

    Create PlaylistSongActivitiesService.js

---
+++ src/services/postgres/PlaylistSongActivitiesService.js
@@ -0,0 +1,45 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+
+const InvariantError = require('../../exceptions/InvariantError');
+
+class PlaylistSongActivities {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addPlaylistSongActivity(playlistId, songId, userId, action) {
+    const id = `activity-${nanoid(16)}`;
+    const time = new Date().toISOString();
+
+    const query = {
+      text: 'INSERT INTO playlist_song_activities VALUES($1, $2, $3, $4, $5) RETURNING id',
+      values: [id, playlistId, songId, userId, action, time],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Activity gagal ditambahkan');
+    }
+  }
+
+  async getPlaylistActivities(playlistId) {
+    const query = {
+      text: `SELECT users.username, songs.title, playlist_song_activities.action, playlist_song_activities.time
+             FROM playlist_song_activities
+             JOIN users ON users.id = playlist_song_activities.user_id
+             JOIN songs ON songs.id = playlist_song_activities.song_id
+             WHERE playlist_song_activities.playlist_id = $1
+             ORDER BY playlist_song_activities.time ASC`,
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(query);
+    return {
+      playlistId,
+      activities: result.rows,
+    };
+  }
+}
+
+module.exports = PlaylistSongActivities;
